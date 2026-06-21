03292f0e99e41716868c0889a3d7c6fcee21f786
ThEndGuy <livm04@hotmail.com>
2026-06-21 04:22:43 +0100

    Added user crop input and writable text boxes

--- main.c
+++ main.c
@@ -18,6 +18,8 @@ static const int IMG_MAX_HEIGHT = 720;
 static const int IMG_X_OFFSET = 20;
 static const int FPS = 60;
 static const int PADDING = 5;
+static const int TEXT_BOX_W_PADDING = 5;
+static const int TEXT_BOX_H_PADDING = 8;
 
 static const Color BG_COLOR = {0x18, 0x18, 0x18, 0xFF};
 static const Color VIEW_COLOR = {0x10, 0x10, 0x10, 0xFF};
@@ -25,10 +27,14 @@ static const Color VIEW_COLOR = {0x10, 0x10, 0x10, 0xFF};
 #define BUTTON_WIDTH 120
 #define BUTTON_HEIGHT 30
 #define BUTTON_PADDING 10
-#define SELECT_WIDTH 225
-#define SELECT_HEIGHT 350
+// #define SELECT_WIDTH 225
+// #define SELECT_HEIGHT 300
 #define SELECT_RES ((float)SELECT_WIDTH / SELECT_HEIGHT)
+
+#define MAX_INPUT_CHARS 100
+
 #define DEBUG_INFO true
+
 float globalScale = 1.0f;
 
 
@@ -40,6 +46,23 @@ Color FADED_BLACK = {50, 50, 50, 100};
 
 // int WINDOW_RESOLUTION = WINDOW_WIDTH/WINDOW_HEIGHT;
 
+typedef struct {
+    Rectangle text_box;
+    char *string;
+    int letter_count;
+    int max_letter_count;
+    int font_size;
+
+    int mouse_cursor;
+
+    int backspace_frames;
+
+    char str[MAX_INPUT_CHARS + 1];
+
+    bool hovering;
+    bool writable;
+    int blink_frames;
+} WritableTextBox;
 
 typedef struct {
     Image image;
@@ -57,6 +80,9 @@ typedef struct {
 } TextureView;
 
 typedef struct {
+    int width;
+    int height;
+
     Rectangle rect;
     Vector2 center;
 
@@ -66,6 +92,12 @@ typedef struct {
     bool attached;
     Vector2 attach_offset;
 } SelectionRect;
+
+int GetNumFromWBox(WritableTextBox wtxtbx) {
+
+    return (int)strtol(wtxtbx.str, NULL, 10);
+
+}
 
 float aspect_ratio(Vector2 obj){
     return obj.x / obj.y;
@@ -139,12 +171,12 @@ Image query_and_load_img_from_file() {
 }
 
 
-bool save_select_as_img(TextureView texV, Rectangle crop_rect, const char* fileName){
+bool save_select_as_img(TextureView texV, Rectangle crop_rect, const char* fileName, int width, int height){
 
-    RenderTexture2D rendered = LoadRenderTexture(SELECT_WIDTH, SELECT_HEIGHT);
+    RenderTexture2D rendered = LoadRenderTexture(width, height);
     BeginTextureMode(rendered);
     ClearBackground(BLANK);
-    Rectangle img_prev = {0, 0, SELECT_WIDTH, SELECT_HEIGHT};
+    Rectangle img_prev = {0, 0, width, height};
 
     DrawTexturePro(texV.texture, crop_rect, img_prev, (Vector2){0, 0}, 0, WHITE);
 
@@ -160,13 +192,13 @@ bool save_select_as_img(TextureView texV, Rectangle crop_rect, const char* fileN
     return true;
 }
 
-bool query_and_save_select_as_img(TextureView texV, Rectangle crop_rect){
+bool query_and_save_select_as_img(TextureView texV, Rectangle crop_rect, int width, int height){
 
     const char *filters[] = {"*.png", "*.jpg"};
     const char *fileName = tinyfd_saveFileDialog("Save image to:", "./", ARR_SIZE(filters), filters, NULL);
     if (!fileName) {return false;}
 
-    if (!save_select_as_img(texV, crop_rect, fileName)) {return false;}
+    if (!save_select_as_img(texV, crop_rect, fileName, width, height)) {return false;}
     return true;
 
 }
@@ -221,6 +253,62 @@ void setup_texture(TextureView *texV){
 
 
 
+void DrawWritableTextBox(WritableTextBox *wtxtbx) {    
+    wtxtbx->font_size = wtxtbx->text_box.height - 10;
+    if (CheckCollisionPointRec(GetMousePosition(), wtxtbx->text_box)) {
+        wtxtbx->hovering = true;
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {wtxtbx->writable = true;}
+    } else {
+        wtxtbx->hovering = false;
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {wtxtbx->writable = false;}
+    }
+    if (IsKeyPressed(KEY_ENTER)) {wtxtbx->writable = false;} // Leave if pressed enter
+
+    if (wtxtbx->writable){
+        int key = GetCharPressed();
+        while (key > 0) { 
+            wtxtbx->str[wtxtbx->letter_count] = (char)key;
+            wtxtbx->str[wtxtbx->letter_count + 1] = '\0';
+            int next_width = MeasureText(wtxtbx->str, wtxtbx->font_size);
+            if (next_width < (wtxtbx->text_box.width - TEXT_BOX_W_PADDING)){
+                wtxtbx->letter_count++;
+            } else {
+                wtxtbx->str[wtxtbx->letter_count] = '\0';
+            }
+            key = GetCharPressed();
+        }
+        if (IsKeyPressed(KEY_BACKSPACE)){ // Erase char
+            wtxtbx->letter_count--;
+            if (wtxtbx->letter_count < 0) {wtxtbx->letter_count = 0;}
+            wtxtbx->str[wtxtbx->letter_count] = '\0';
+        }
+        if (IsKeyDown(KEY_BACKSPACE)) { // Hold backspace behavior
+            wtxtbx->backspace_frames++;
+            if (wtxtbx->backspace_frames > 40 && wtxtbx->backspace_frames % 5 == 0) {
+                wtxtbx->letter_count--;
+                if (wtxtbx->letter_count < 0) {wtxtbx->letter_count = 0;}
+                wtxtbx->str[wtxtbx->letter_count] = '\0';
+            }
+
+        } else {
+            wtxtbx->backspace_frames = 0;
+
+        }
+    }
+    DrawRectangleRec(wtxtbx->text_box, WHITE);
+    int tx = (int)wtxtbx->text_box.x;
+    int ty = (int)wtxtbx->text_box.y;
+    DrawText(wtxtbx->str, tx + TEXT_BOX_W_PADDING, ty + TEXT_BOX_H_PADDING, wtxtbx->font_size, MAROON);
+    if (wtxtbx->writable) {wtxtbx->blink_frames++;} else {wtxtbx->blink_frames=0;}
+    if ((((wtxtbx->blink_frames / 30) + 1) % 2) == 0) {
+        DrawText(
+            "|",
+            tx + 8 + MeasureText(wtxtbx->str, wtxtbx->font_size),
+            ty + 8, wtxtbx->font_size,
+            MAROON); 
+    }
+}
+
 
 
 int main(int argc, char* argv[])
@@ -240,7 +328,6 @@ int main(int argc, char* argv[])
         IMG_MAX_HEIGHT + 2*PADDING
         };
     
-    
     TextureView main_tex = {0};
     SelectionRect selection = {0};
     if (argc > 1){
@@ -252,7 +339,7 @@ int main(int argc, char* argv[])
     }
 
     // Selection rect settings
-    selection.rect =  (Rectangle){0, 0, 225, 350};
+    selection.rect =  (Rectangle){0, 0, 225, 300};
     selection.center = (Vector2){
         selection.rect.x + selection.rect.width/2.0f,
         selection.rect.y + selection.rect.height/2.0f,
@@ -260,11 +347,25 @@ int main(int argc, char* argv[])
     selection.scale = 1.0f;
     selection.target_scale = 1.0f;
     selection.attached = false;
+
+    bool mouse_is_hovering = false; // If mouse is hovering any text box
+
+    WritableTextBox width_wtxtbx = {0};
+    width_wtxtbx.text_box = (Rectangle){GetScreenWidth()-350, 500, 100, 40};
+    strcpy(width_wtxtbx.str, "225");
+    width_wtxtbx.letter_count = strlen(width_wtxtbx.str); 
+    selection.width = GetNumFromWBox(width_wtxtbx);
+
+    WritableTextBox height_wtxtbx = {0};
+    height_wtxtbx.text_box = (Rectangle){GetScreenWidth()-350, 550, 100, 40};
+    strcpy(height_wtxtbx.str, "300");
+    height_wtxtbx.letter_count = strlen(height_wtxtbx.str); 
+    selection.height = GetNumFromWBox(height_wtxtbx);
     
     setup_texture(&main_tex);
     SetTargetFPS(FPS);
-    while (!WindowShouldClose()) {
-        if (IsKeyPressed(KEY_Q)) {CloseWindow();}
+    while (!WindowShouldClose()) { // MAIN LOOP START
+        if (IsKeyPressed(KEY_Q) && !width_wtxtbx.writable) {CloseWindow();}
 
         BeginDrawing();
         ClearBackground(BG_COLOR);
@@ -274,14 +375,15 @@ int main(int argc, char* argv[])
 
         // Selection rectangle logic
         // Using keys:
-       
-        int key_offset;
-        if (IsKeyDown(KEY_LEFT_SHIFT)){key_offset = 10;}else{key_offset = 1;}
+        if (!width_wtxtbx.writable) {
+            int key_offset;
+            if (IsKeyDown(KEY_LEFT_SHIFT)){key_offset = 10;}else{key_offset = 1;}
 
-        if (IsKeyPressed(KEY_D) || IsKeyPressed(KEY_RIGHT)) {selection.center.x += key_offset;} 
-        if (IsKeyPressed(KEY_A) || IsKeyPressed(KEY_LEFT))  {selection.center.x -= key_offset;}
-        if (IsKeyPressed(KEY_S) || IsKeyPressed(KEY_DOWN))  {selection.center.y += key_offset;} 
-        if (IsKeyPressed(KEY_W) || IsKeyPressed(KEY_UP))    {selection.center.y -= key_offset;} 
+            if (IsKeyPressed(KEY_D) || IsKeyPressed(KEY_RIGHT)) {selection.center.x += key_offset;} 
+            if (IsKeyPressed(KEY_A) || IsKeyPressed(KEY_LEFT))  {selection.center.x -= key_offset;}
+            if (IsKeyPressed(KEY_S) || IsKeyPressed(KEY_DOWN))  {selection.center.y += key_offset;} 
+            if (IsKeyPressed(KEY_W) || IsKeyPressed(KEY_UP))    {selection.center.y -= key_offset;} 
+        }
 
         float dt = GetFrameTime();
         float wheel = GetMouseWheelMove();
@@ -329,7 +431,7 @@ int main(int argc, char* argv[])
 
             // Stop anchoring
             if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT) || IsMouseButtonReleased(MOUSE_RIGHT_BUTTON)){selection.attached = false;}
-            if (!is_inside_img(mouse_pos, main_tex.as_rect)){
+            if (!is_inside_img(mouse_pos, view_rect)){
                 selection.center.x = selection.rect.x + selection.rect.width/2.0f;
                 selection.center.y = selection.rect.y + selection.rect.height/2.0f;
 
@@ -342,8 +444,8 @@ int main(int argc, char* argv[])
 
             selection.target_scale += wheel * 0.1f;
 
-            float sel_max_scale_x = (float)main_tex.image.width  / SELECT_WIDTH;
-            float sel_max_scale_y = ((float)main_tex.image.height) / SELECT_HEIGHT;
+            float sel_max_scale_x = view_rect.width / selection.width;
+            float sel_max_scale_y = view_rect.height / selection.height;
             float max_scale = (sel_max_scale_x < sel_max_scale_y) ? sel_max_scale_x : sel_max_scale_y;
             if (selection.target_scale > max_scale){
             selection.target_scale = max_scale;
@@ -357,11 +459,10 @@ int main(int argc, char* argv[])
         if (IsKeyPressed(KEY_F5)) {setup_texture(&main_tex);}
         selection.scale += (selection.target_scale - selection.scale) * 20.0f * dt;
 
-        selection.rect.width = SELECT_WIDTH * selection.scale;
-        selection.rect.height = SELECT_HEIGHT * selection.scale;
+        selection.rect.width = selection.width * selection.scale;
+        selection.rect.height = selection.height * selection.scale;
         selection.rect.x = selection.center.x - selection.rect.width/2.0f;
         selection.rect.y = selection.center.y - selection.rect.height/2.0f;
-        if (selection.rect.height > main_tex.image.height){selection.rect.height = main_tex.image.height;}
 
         force_rect_inside_view(&selection, view_rect);
 
@@ -387,9 +488,9 @@ int main(int argc, char* argv[])
         // Refresh image button
         if (GuiButton((Rectangle){100 + BUTTON_WIDTH + BUTTON_PADDING, GetScreenHeight() - 100, BUTTON_WIDTH, BUTTON_HEIGHT}, "#012# Reset position")) {setup_texture(&main_tex);}
 
-        Rectangle img_preview = {GetScreenWidth() - 410, 100, SELECT_WIDTH, SELECT_HEIGHT};
+        Rectangle img_preview = {GetScreenWidth() - 410, 100, selection.width, selection.height};
         // SAVE IMAGE BUTTON
-        if (IsImageValid(main_tex.image)) {if (GuiButton((Rectangle){img_preview.x + img_preview.width/2.0f - BUTTON_WIDTH/2.0f, img_preview.y + img_preview.height + 20, BUTTON_WIDTH, BUTTON_HEIGHT }, "#002# Save as Image")) query_and_save_select_as_img(main_tex, crop_rect);}
+        if (IsImageValid(main_tex.image)) {if (GuiButton((Rectangle){img_preview.x + img_preview.width/2.0f - BUTTON_WIDTH/2.0f, img_preview.y + img_preview.height + 20, BUTTON_WIDTH, BUTTON_HEIGHT }, "#002# Save as Image")) query_and_save_select_as_img(main_tex, crop_rect, selection.width, selection.height);}
 
         // Rectangle stuff
 
@@ -408,12 +509,26 @@ int main(int argc, char* argv[])
         DrawRectangleRec(selection.rect, SELECT_COLOR);
         DrawRectangleLinesEx(selection.rect, 1, BLACK);
         draw_thirds_lines(&selection.rect);
+
+        DrawWritableTextBox(&width_wtxtbx);
+        DrawWritableTextBox(&height_wtxtbx);
+        
+        if (IsKeyPressed(KEY_ENTER)){
+            selection.width = GetNumFromWBox(width_wtxtbx);
+            selection.height = GetNumFromWBox(height_wtxtbx);
+        }
+        mouse_is_hovering = false;
+        mouse_is_hovering |= width_wtxtbx.hovering;
+        mouse_is_hovering |= height_wtxtbx.hovering;
+        SetMouseCursor(mouse_is_hovering ? MOUSE_CURSOR_IBEAM : MOUSE_CURSOR_DEFAULT);
+        
         if (DEBUG_INFO) {
             DrawText(TextFormat("moup: x=%.2f, y=%.2f", mouse_pos.x, mouse_pos.y), GetScreenWidth()-500, GetScreenHeight()/2 + 160, 20, WHITE);
             DrawText(TextFormat("mt.tp: x=%.2f, y=%.2f", main_tex.texture_pos.x, main_tex.texture_pos.y), GetScreenWidth()-500, GetScreenHeight()/2 + 180, 20, WHITE);
             DrawText(TextFormat("ao: x=%.2f, y=%.2f", main_tex.attach_offset.x, main_tex.attach_offset.y), GetScreenWidth()-500, GetScreenHeight()/2 + 200, 20, WHITE);
             DrawText(TextFormat("mt.a: %b", main_tex.attached), GetScreenWidth()-500, GetScreenHeight()/2 + 220, 20, WHITE);
             DrawText(TextFormat("mt.sc: %f", main_tex.target_scale), GetScreenWidth()-500, GetScreenHeight()/2 + 240, 20, WHITE);
+            DrawText(TextFormat("selsize: w=%.2f h=%.2f", selection.rect.width, selection.rect.height), GetScreenWidth()-500, GetScreenHeight()/2 + 260, 20, WHITE);
         }
 
         if (IsWindowResized()){
